f3b6f5cce6d27d3b540b6fc3c1857f86f1c6ca84
Daria <daslebedyuk@gmail.com>
2025-04-01 20:46:47 +0300

    Авторизация и регистрация с закреплением session и комментарии

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+
 app = Flask(__name__) # Создание приложение. Так у нас flask знает где искать ресурсы(шаблоны и статические файлы)
 app.secret_key = "super_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) # Оборачиваем наше приложение в класс SQLAlchemy
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+class User(db.Model): # Создание таблиц
+    id = db.Column(db.Integer, primary_key=True) # Создание столбца id со значением integer. primary_key=True - означает, что столбцы будут идти по нумерации 1, 2, 3...
+    login = db.Column(db.String(80), nullable=False) # Создание столбца login со значением String с ограничением 80 символов. nullable=False - означает, что если ничего не введется, то будет возвращать False
+    password = db.Column(db.String(255), nullable=False) # Здесь ограничение 255, потому что у нас идёт шифрование пароля в бд.
 
-with app.app_context():
-    db.create_all()
+with app.app_context(): # with - это по сути контекстное управление.  app.app_context() - создание контекста, без него нельзя взаимодействовать с базой данных
+    db.create_all() # Создание таблиц, если их ещё нет. Эта штука взаимодействует с моделями, например сейчас с классом User
 
 @app.route("/") # декоратор, чтобы сообщить Flask, какой URL должен вызывать нашу функцию.
 def index():
@@ -22,6 +27,8 @@ def index():
 
 @app.route("/profile.html")
 def profile():
+    if "username" not in session: # Если наш аккаунт не сохранен в сессии(бд), то переходим на страницу login
+        return redirect("/login.html")
     return render_template('profile.html')
 
 
@@ -42,40 +49,41 @@ def register():
 
 @app.route('/register_handler', methods = ['POST', 'GET']) # Напрямую связываемся с front, '/register_handler' - это обработчик(action) в register.html. С methods идентичная ситуация
 def reg_hand():
-    if request.method == 'POST':
-        username = request.form['username']
+    if request.method == 'POST': # Получение данных формы, после сохраняем в бд
+        username = request.form['username'] # Обращение к front, а именно к конкретному полю
         password = request.form['password']
-        existing_user = User.query.filter_by(login=username).first()
+        existing_user = User.query.filter_by(login=username).first() # Здесь мы делаем проверку, есть ли уже такой пользователь
         if existing_user:
             return "Ошибка: Такой пользователь уже есть!"
 
-
-        new_user = User(login=username, password=generate_password_hash(password))
+        hashed_password = generate_password_hash(password)
+        new_user = User(login=username, password=hashed_password) # А тут создаём пользователя с передачей login и password. generate_password_hash(password) - это хэширование пароля
         db.session.add(new_user)
-        db.session.commit()
+        db.session.commit() # Сохранение изменений в бд
 
         return redirect('/login.html')
 
 
-@app.route('/login_handler', methods = ['POST', 'GET'])
+@app.route('/login_handler', methods = ['POST'])
 def login_hand():
-    if "username" in session:
-        return redirect('/profile.html')
+    username = request.form['username']
+    password = request.form['password']
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    user = User.query.filter_by(login=username).first()  # Находим запись в бд, по тому что ввели
 
-        user = User.query.filter_by(login=username).first()
-        if user and check_password_hash(user.password, password):
-            session['username'] = user.id
-            return redirect('/profile.html')
+    if user is None: # Если user равен None, значит, пользователя с таким логином нет в базе данных.
+        return "Ошибка: Пользователь не найден!"
 
-        return 'Ошибка: Неверный логин или пароль!'
+    if not check_password_hash(user.password, password):  # Проверка пароля на хэширование
+        return "Ошибка: Неверный пароль!"
 
-    return render_template("login.html")
+    session['username'] = user.login # Если логин и пароль верны, мы сохраняем в сессию, и пользователь остается авторизованным на сайте
+    return redirect('/profile.html')
+
+
 
 
 
 if __name__ == '__main__': # Делаем так, чтобы у нас всё автоматически подтягивалось при изменении чего либо
     app.run(debug=True)
+
